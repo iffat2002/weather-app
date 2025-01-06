95cdadedeeb161afd8ec462b2bdc7e196463d8f0
iffat2002 <113204563+iffat2002@users.noreply.github.com>
2025-01-07 02:08:33 +0500

    commit

---
+++ weather-app-main/precache-manifest.9879f55c9c565486b88431657d6003da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df769ac3b506fc6057a402296d82590a",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3e6cacce86d77dd5fe3c",
+    "url": "/weather-app/static/css/main.8d7cf30a.chunk.css"
+  },
+  {
+    "revision": "71cb4bad829785da214f",
+    "url": "/weather-app/static/js/2.e0cdfcab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.e0cdfcab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e6cacce86d77dd5fe3c",
+    "url": "/weather-app/static/js/main.5a86eca6.chunk.js"
+  },
+  {
+    "revision": "1740056d64fcf29620db",
+    "url": "/weather-app/static/js/runtime-main.be244a54.js"
+  },
+  {
+    "revision": "8cec250e96fe3ef0202f8115640c1a86",
+    "url": "/weather-app/static/media/Scattered Clouds.8cec250e.png"
+  },
+  {
+    "revision": "c67755ac6475a06e3075bb6c91dccd55",
+    "url": "/weather-app/static/media/Smoke.c67755ac.jpg"
+  },
+  {
+    "revision": "52662f24ae8da51ecc58f4358b2399fc",
+    "url": "/weather-app/static/media/city scape.52662f24.png"
+  },
+  {
+    "revision": "410560155f7cad20c51c2d5dd829544b",
+    "url": "/weather-app/static/media/ibrahim.41056015.jpg"
+  },
+  {
+    "revision": "95d72d5ca4d4fab9fa7b7ec512cdf8c1",
+    "url": "/weather-app/static/media/question.95d72d5c.png"
+  }
+]);
